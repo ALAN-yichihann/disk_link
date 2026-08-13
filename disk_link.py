f9d6c19b1f73fff926a95ecdc69618552bda554a
ALAN-yichihann <3874097592@qq.com>
2026-08-13 23:05:19 +0800

    独立语言文件

--- disk_link.py
+++ disk_link.py
@@ -2,11 +2,10 @@ import os
 import sys
 import json
 
-from output import print_info, print_warning, print_error, input_confirm
 from typing import List, Any
 from datetime import datetime
 
-from storage import Storage
+from output import print_info, print_warning, print_error, input_confirm
 
 class DiskLink:
     """管理程序的类"""
@@ -23,25 +22,9 @@ class DiskLink:
         print("正在初始化...")
         print("Initializing...\n")
 
-        self.STORAGE = Storage()
-
-        # 设置中英文提示语
-        self.CN_PROMPTS = self.STORAGE.CN_PROMPTS
-        self.EN_PROMPTS = self.STORAGE.EN_PROMPTS
-
-        # 设置中英文命令提示语
-        self.COMMAND_HELPS_CN = self.STORAGE.COMMAND_HELPS_CN
-        self.COMMAND_HELPS_EN = self.STORAGE.COMMAND_HELPS_EN
-
-        # 设置中英文全部命令提示语
-        self.ALL_HELPS_CN = self.STORAGE.ALL_HELPS_CN
-        self.ALL_HELPS_EN = self.STORAGE.ALL_HELPS_EN
-
         # 默认语言为中文
         self.language = "cn"
-        self.prompts = self.CN_PROMPTS
-        self.command_helps = self.COMMAND_HELPS_CN
-        self.all_helps = self.ALL_HELPS_CN
+        self.load_language(self.language)
 
         # 设置命令
         self.COMMANDS = ('language', 'quit', 'help', "append", "del", "list", "jump", "check")
@@ -119,7 +102,7 @@ class DiskLink:
             # 输出报错
             else:
                 self.fun_num = 0
-                print_warning(self.prompts[1])
+                print_warning(self.prompts["INVALID_PROMPT"])
 
     def read_texts(self, first_time=False):
         """读取应用和路径"""
@@ -146,7 +129,7 @@ class DiskLink:
         try:
             os.startfile(self.DISK_NAME + self.appls[self.usr[0]])
         except FileNotFoundError:
-            print_warning(self.prompts[27])
+            print_warning(self.prompts["PROGRAM_MOVED_PROMPT"])
 
     def run_command(self):
         """执行命令"""
@@ -184,7 +167,7 @@ class DiskLink:
 
         # 输出报错
         else:
-            print(self.prompts[1])
+            print(self.prompts["INVALID_PROMPT"])
 
     def _max_len(self):
         """求列表中所有字符串最长值，默认值10"""
@@ -221,12 +204,12 @@ class DiskLink:
 
         # 连续十次就输出并归零
         if self.fun_num == 10:
-            print_warning(self.prompts[3])
+            print_warning(self.prompts["JOKE_PROMPT"])
             self.fun_num = 0
 
     def read_usr(self):
         """读取和分割用户输入"""
-        self.usr = input(self.prompts[0])
+        self.usr = input(self.prompts["ENTRY_PROMPT"])
 
         # 列表化用户输入
         usr = list(self.usr)
@@ -290,34 +273,52 @@ class DiskLink:
         """切换语言"""
         # 中文
         if self.usr[1] == 'cn':
-            self.language = "cn"
-            self.prompts = self.CN_PROMPTS
-            self.command_helps = self.COMMAND_HELPS_CN
-            self.all_helps = self.ALL_HELPS_CN
-            print_info(self.prompts[2])
+            self.load_language('cn')
+            print_info(self.prompts["LANGUAGE_CN_PROMPT"])
 
         # 英文
         elif self.usr[1] == 'en':
-            self.language = "en"
-            self.prompts = self.EN_PROMPTS
-            self.command_helps = self.COMMAND_HELPS_EN
-            self.all_helps = self.ALL_HELPS_EN
-            print_info(self.prompts[2])
+            self.load_language('en')
+            print_info(self.prompts["LANGUAGE_EN_PROMPT"])
 
         # 报错
         else:
-            print_warning(self.prompts[1])
+            print_warning(self.prompts["INVALID_PROMPT"])
+
+    def load_language(self, lang):
+        """加载语言"""
+        # 中文
+        if lang == 'cn':
+            self.language = "cn"
+            with open("lang/zh-CN.json", "r", encoding="utf-8") as f:
+                dic = json.load(f)
+                self.prompts = dic["PROMPTS"] # type: ignore
+                self.command_helps = dic["COMMAND_HELPS"] # type: ignore
+                self.all_helps = dic["ALL_COMMANDS_FORMATTED"] # type: ignore
+
+        # 英文
+        elif lang == 'en':
+            self.language = "en"
+            with open("lang/en.json", "r", encoding="utf-8") as f:
+                dic = json.load(f)
+                self.prompts = dic["PROMPTS"] # type: ignore
+                self.command_helps = dic["COMMAND_HELPS"] # type: ignore
+                self.all_helps = dic["ALL_COMMANDS_FORMATTED"] # type: ignore
+
+        # 报错
+        else:
+            print_warning(self.prompts["INVALID_PROMPT"])
 
     def print_help(self):
         """输出帮助"""
         # 输出全部帮助
         if len(self.usr) == 1:
             # 输出
-            print_info(self.all_helps)
+            self.print_all_helps()
 
         # 输出单个命令帮助
         elif len(self.usr) == 2 and self.usr[1] in self.COMMANDS:
-            print_info(self.command_helps[self.COMMANDS.index(self.usr[1])])
+            print_info(self.command_helps[self.usr[1]])
 
         # 输出单个程序及位置
         elif len(self.usr) == 2 and self.usr[1] in self.appls.keys():
@@ -325,13 +326,19 @@ class DiskLink:
 
         # 报错
         else:
-            print_warning(self.prompts[1])
+            print_warning(self.prompts["INVALID_PROMPT"])
+
+    def print_all_helps(self):
+        """输出所有帮助"""
+        print_info(self.prompts["ALL_COMMANDS_HEAD_PROMPT"])
+        for help_text in self.all_helps:
+            print_info(help_text)
 
     def append_appl(self):
         """添加程序快捷方式"""
         # 检查格式 append [appl] at [dir]
         if not len(self.usr) == 4 or not self.usr[2] == "at":
-            print_warning(self.prompts[1])
+            print_warning(self.prompts["INVALID_PROMPT"])
 
         # 处理
         else:
@@ -346,31 +353,32 @@ class DiskLink:
 
             # 检查程序是否存在
             if not os.path.exists(new_dir):
-                print_warning(self.prompts[5])
+                print_warning(self.prompts["NOT_FOUND_UNDER_ROUTE_PROMPT"])
 
             # 程序必须在硬盘中
             elif new_dir.split('\\')[0] != self.DISK_NAME:
-                print_warning(self.prompts[7])
+                print_warning(self.prompts["PATH_NOT_ON_DISK_PROMPT"])
 
             # 后缀必须为.exe
             elif new_dir.split('\\')[-1][-3:] != "exe":
-                print_warning(self.prompts[9])
+                print_warning(self.prompts["INVALID_FILE_EXTENSION_PROMPT"])
 
             # 不准重名
             elif new_appl in self.appls.keys():
-                print_warning(self.prompts[4])
+                print_warning(self.prompts["ALREADY_EXIST_PROMPT"])
 
+            # 不准与命令重名
             elif new_appl in self.COMMANDS:
-                print_warning(self.prompts[22])
+                print_warning(self.prompts["NAME_CONFLICT_PROMPT"])
 
             # 不准重复路径
             elif new_dir in exist_dir:
-                print_warning(self.prompts[6])
+                print_warning(self.prompts["DUPLICATE_PATH_PROMPT"])
 
             # 添加
             else:
                 # 询问是否添加
-                confirm = input_confirm(self.prompts[8])
+                confirm = input_confirm(self.prompts["CONFIRM_ADD_SHORTCUT_PROMPT"])
 
                 # 确认
                 if confirm == "Y":
@@ -395,7 +403,7 @@ class DiskLink:
 
                 # 报错
                 else:
-                    print_warning(self.prompts[1])
+                    print_warning(self.prompts["INVALID_PROMPT"])
 
     def del_appl(self):
         """删除程序快捷方式"""
@@ -403,7 +411,7 @@ class DiskLink:
         if self.usr[1] in self.appls.keys():
 
             # 询问确认删除
-            confirm = input_confirm(self.prompts[14])
+            confirm = input_confirm(self.prompts["CONFIRM_DELETE_SHORTCUT_PROMPT"])
 
             # 确认
             if confirm == "Y":
@@ -419,23 +427,23 @@ class DiskLink:
                     self.check_vitals()
 
                 # 提示成功
-                print_info(self.prompts[13])
+                print_info(self.prompts["SUCCESS_DELETE_SHORTCUT_PROMPT"])
 
             # 取消
             elif confirm == "n":
-                print_info(self.prompts[15])
+                print_info(self.prompts["DELETION_ABORTED_PROMPT"])
 
         # 不在已有程序中
         else:
-            print_warning(self.prompts[12])
+            print_warning(self.prompts["PROGRAM_NOT_FOUND_PROMPT"])
 
     def list_appl(self):
         """列出所有程序"""
         if len(self.appls) != 0:
-            print_info(self.prompts[16])
+            print_info(self.prompts["PROGRAM_LIST_PROMPT"])
             self._print_appls(self._max_len())
         else:
-            print_info(self.prompts[23])
+            print_info(self.prompts["NO_SHORTCUTS_PROMPT"])
 
     def jump_route(self):
         """打开对应文件资源管理器页面"""
@@ -448,9 +456,9 @@ class DiskLink:
             try:
                 os.system("explorer " + command)
             except FileNotFoundError:
-                print_warning(self.prompts[27])
+                print_warning(self.prompts["PROGRAM_MOVED_PROMPT"])
         else:
-            print(self.prompts[12])
+            print(self.prompts["PROGRAM_NOT_FOUND_PROMPT"])
 
     def create_save(self):
         """备份"""
@@ -473,7 +481,7 @@ class DiskLink:
                 self.error_inform("01010101(2)")
                 self.handle_error("01010101(2)")
         if command and not error:
-            print_info(self.prompts[26])
+            print_info(self.prompts["NECESSARY_FILES_OK_PROMPT"])
 
 
 
@@ -493,11 +501,11 @@ class DiskLink:
     def error_inform(self, code, method = "inform"):
         """告知错误"""
         if method == "inform":
-            print_error(self.CN_PROMPTS[self.HANDLE_LIST[self.ERROR_LIST.index(code)][0]])
-            print_error(self.EN_PROMPTS[self.HANDLE_LIST[self.ERROR_LIST.index(code)][0]])
+            print_error(self.prompts[self.HANDLE_LIST[self.ERROR_LIST.index(code)][0]])
+            print_error(self.prompts[self.HANDLE_LIST[self.ERROR_LIST.index(code)][0]] + "修改标识符")
         elif method == "solved":
-            print_warning(self.CN_PROMPTS[self.HANDLE_LIST[self.ERROR_LIST.index(code)][1]])
-            print_warning(self.EN_PROMPTS[self.HANDLE_LIST[self.ERROR_LIST.index(code)][1]])
+            print_warning(self.prompts[self.HANDLE_LIST[self.ERROR_LIST.index(code)][1]])
+            print_warning(self.prompts[self.HANDLE_LIST[self.ERROR_LIST.index(code)][1]] + "修改标识符")
 
     def handle_error(self, code):
         """处理错误"""
